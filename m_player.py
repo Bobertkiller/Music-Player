b6873bbe3c3dd8ab255bc57d6193aeeb289c7397
nanometer5088 <code.deleo@simplelogin.com>
2023-01-28 21:09:33 -0300

    PEP8 Compliance

--- m_player.py
+++ m_player.py
@@ -1,44 +1,45 @@
-import tkinter as tk
-from tkinter import filedialog
 import fnmatch
 import os
 import platform
-from pygame import mixer
 import shutil
+import tkinter as tk
+from tkinter import filedialog
+
+from pygame import mixer
 
 canvas = tk.Tk()
-canvas.title('Music Player')
-canvas.geometry('600x800')
-canvas.config(bg='#6433d6')
-if platform.system() == 'Windows':
-    rootpath = ('%USERPROFILE%/Music') # Windows path
-elif platform.system() == 'Darwin':
+canvas.title("Music Player")
+canvas.geometry("600x800")
+canvas.config(bg="#6433d6")
+if platform.system() == "Windows":
+    rootpath = "%USERPROFILE%/Music"  # Windows path
+elif platform.system() == "Darwin":
     try:
-        rootpath = ('~/Music') # Mac path, I think
+        rootpath = "~/Music"  # Mac path, I think
     except:
-        print('Weird Fucking system Bro')
-elif platform.system() == 'Linux':
-    rootpath = ('~/Music') # Linux path
+        print("Weird Fucking system Bro")
+elif platform.system() == "Linux":
+    rootpath = "~/Music"  # Linux path
 pattern = "*.mp3"
 
-prev_img = tk.PhotoImage(file='Docs/assets/prev_img.png')
-stop_img = tk.PhotoImage(file='Docs/assets/stop_img.png')
-play_img = tk.PhotoImage(file='Docs/assets/play_img.png')
-pause_img = tk.PhotoImage(file='Docs/assets/pause_img.png')
-next_img = tk.PhotoImage(file='Docs/assets/next_img.png')
+prev_img = tk.PhotoImage(file="Docs/assets/prev_img.png")
+stop_img = tk.PhotoImage(file="Docs/assets/stop_img.png")
+play_img = tk.PhotoImage(file="Docs/assets/play_img.png")
+pause_img = tk.PhotoImage(file="Docs/assets/pause_img.png")
+next_img = tk.PhotoImage(file="Docs/assets/next_img.png")
 
 mixer.init()
 
 
 def select():
-    label.config(text=listBox.get('anchor'))
-    mixer.music.load(rootpath + '\\' + listBox.get('anchor'))
+    label.config(text=listBox.get("anchor"))
+    mixer.music.load(rootpath + "\\" + listBox.get("anchor"))
     mixer.music.play()
 
 
 def stop():
     mixer.music.stop()
-    listBox.select_clear('active')
+    listBox.select_clear("active")
 
 
 def play_next():
@@ -47,10 +48,10 @@ def play_next():
     next_song_name = listBox.get(next_song)
     label.config(text=next_song_name)
 
-    mixer.music.load(rootpath + '\\' + next_song_name)
+    mixer.music.load(rootpath + "\\" + next_song_name)
     mixer.music.play()
 
-    listBox.select_clear(0, 'end')
+    listBox.select_clear(0, "end")
     listBox.activate(next_song)
     listBox.select_set(next_song)
 
@@ -61,67 +62,106 @@ def play_prev():
     prev_song_name = listBox.get(prev_song)
     label.config(text=prev_song_name)
 
-    mixer.music.load(rootpath + '\\' + prev_song_name)
+    mixer.music.load(rootpath + "\\" + prev_song_name)
     mixer.music.play()
 
-    listBox.select_clear(0, 'end')
+    listBox.select_clear(0, "end")
     listBox.activate(prev_song)
     listBox.select_set(prev_song)
 
 
 def pause_song():
-    if pauseButton['text'] == 'Pause':
+    if pauseButton["text"] == "Pause":
         mixer.music.pause()
-        pauseButton['text'] = 'Play'
+        pauseButton["text"] = "Play"
     else:
         mixer.music.unpause()
-        pauseButton['text'] = 'Pause'
+        pauseButton["text"] = "Pause"
+
 
 def add_song():
     filepath = filedialog.askopenfilename()
-    shutil.copy2(filepath,rootpath)
+    shutil.copy2(filepath, rootpath)
+
 
 def volume():
     pass
 
 
-listBox = tk.Listbox(canvas, fg='cyan', bg='black',
-                     width=100, font=('ds-digital', 15))
+listBox = tk.Listbox(
+    canvas, fg="cyan", bg="black", width=100, font=("ds-digital", 15)
+)
 listBox.pack(padx=15, pady=15)
 
-label = tk.Label(canvas, text='', bg='#6433d6',
-                 fg='yellow', font=('cantarell', 18))
+label = tk.Label(
+    canvas, text="", bg="#6433d6", fg="yellow", font=("cantarell", 18)
+)
 label.pack(pady=15)
 
-top = tk.Frame(canvas, bg='#6433d6')
-top.pack(padx=10, pady=5, anchor='center')
+top = tk.Frame(canvas, bg="#6433d6")
+top.pack(padx=10, pady=5, anchor="center")
 
-prevButton = tk.Button(canvas, text='Prev', image=prev_img,
-                       bg='#6433d6', borderwidth=0, command=play_prev)
-prevButton.pack(pady=15, in_=top, side='left')
+prevButton = tk.Button(
+    canvas,
+    text="Prev",
+    image=prev_img,
+    bg="#6433d6",
+    borderwidth=0,
+    command=play_prev,
+)
+prevButton.pack(pady=15, in_=top, side="left")
 
-stopButton = tk.Button(canvas, text='Stop', image=stop_img,
-                       bg='#6433d6', borderwidth=0, command=stop)
-stopButton.pack(pady=15, in_=top, side='left')
+stopButton = tk.Button(
+    canvas,
+    text="Stop",
+    image=stop_img,
+    bg="#6433d6",
+    borderwidth=0,
+    command=stop,
+)
+stopButton.pack(pady=15, in_=top, side="left")
 
-playButton = tk.Button(canvas, text='Play', image=play_img,
-                       bg='#6433d6', borderwidth=0, command=select)
-playButton.pack(pady=15, in_=top, side='left')
+playButton = tk.Button(
+    canvas,
+    text="Play",
+    image=play_img,
+    bg="#6433d6",
+    borderwidth=0,
+    command=select,
+)
+playButton.pack(pady=15, in_=top, side="left")
 
-pauseButton = tk.Button(canvas, text='Pause',
-                        image=pause_img, bg='#6433d6', borderwidth=0, command=pause_song)
-pauseButton.pack(pady=15, in_=top, side='left')
+pauseButton = tk.Button(
+    canvas,
+    text="Pause",
+    image=pause_img,
+    bg="#6433d6",
+    borderwidth=0,
+    command=pause_song,
+)
+pauseButton.pack(pady=15, in_=top, side="left")
 
-nextButton = tk.Button(canvas, text='Next', image=next_img,
-                       bg='#6433d6', borderwidth=0, command=play_next)
-nextButton.pack(pady=15, in_=top, side='left')
+nextButton = tk.Button(
+    canvas,
+    text="Next",
+    image=next_img,
+    bg="#6433d6",
+    borderwidth=0,
+    command=play_next,
+)
+nextButton.pack(pady=15, in_=top, side="left")
 
-addButton = tk.Button(canvas, text='Add song',
-                       bg='#6433d6', borderwidth=0, command=add_song)
-addButton.pack(pady=15, side='left')
+addButton = tk.Button(
+    canvas,
+    text="Add song",
+    bg="#6433d6",
+    borderwidth=0,
+    command=add_song,
+)
+addButton.pack(pady=15, side="left")
 
 for root, dirs, files in os.walk(rootpath):
     for filename in fnmatch.filter(files, pattern):
-        listBox.insert('end', filename)
+        listBox.insert("end", filename)
 
 canvas.mainloop()
--- old.py
+++ old.py
@@ -1,41 +1,45 @@
+# # Versão 0.2
+
+# from tkinter import *
+# from tkinter import filedialog, ttk
+
+# import playsound as ps
 
 
-'''
-Versão 0.2
+# def abra_arquivo():
+#     filepath = filedialog.askopenfilename()
+#     print(filepath)
+#     ps.playsound(filepath)
 
-import playsound as ps
-from tkinter import *
-from tkinter import ttk
-from tkinter import filedialog
 
-def abra_arquivo():
-  filepath = filedialog.askopenfilename()
-  print(filepath)
-  ps.playsound(filepath)
+# root = Tk()
+# frm = ttk.Frame(root, padding=10)
+# frm.grid()
+# ttk.Label(frm, text="Escolha o audio!").grid(column=0, row=0)
+# ttk.Button(frm, text="Open", command=abra_arquivo).grid(
+#     column=1, row=0
+# )
+# ttk.Button(frm, text="Close", command=root.destroy).grid(
+#     column=1, row=1
+# )
+# root.mainloop()
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Escolha o audio!").grid(column=0, row=0)
-ttk.Button(frm, text="Open", command=abra_arquivo).grid(column=1, row=0)
-ttk.Button(frm, text='Close', command=root.destroy).grid(column=1,row=1)
-root.mainloop()
-'''
 
-'''
-#Versão 0.1
+# # Versão 0.1
 
-import playsound as ps
-from tkinter import *
-from tkinter import filedialog
+# from tkinter import *
+# from tkinter import filedialog
 
-def abra_arquivo():
-  filepath = filedialog.askopenfilename()
-  print(filepath)
-  ps.playsound(filepath)
- 
-janela = Tk()
-botao = Button(text='Open', command=abra_arquivo)
-botao.pack()
-janela.mainloop()
-'''
+# import playsound as ps
+
+
+# def abra_arquivo():
+#     filepath = filedialog.askopenfilename()
+#     print(filepath)
+#     ps.playsound(filepath)
+
+
+# janela = Tk()
+# botao = Button(text="Open", command=abra_arquivo)
+# botao.pack()
+# janela.mainloop()
